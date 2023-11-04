e5f09524df7cee6f5410298a468d50f8a1bb3608
JuyeongWoo <dlsrtyex63@gmail.com>
2023-11-05 03:47:35 +0900

    chore: 버튼 추가 및 css 수정

--- src/pages/OAuth2Signin/OAuth2Signin.jsx
+++ src/pages/OAuth2Signin/OAuth2Signin.jsx
@@ -1,92 +1,32 @@
 import React from "react";
 import { Reset } from "styled-reset";
-import { css } from "@emotion/react";
 /** @jsxImportSource @emotion/react */
-
-const mainLayout = css`
-    display: grid;
-    place-items: center; //자기 요소의 내부 요소가 1개일 경우에만 적용가능
-    width: 70%;
-    min-height: 100vh;
-    margin: 0 auto;
-    background-color: #fffbf5;
-    box-shadow:
-        rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
-        rgba(10, 37, 64, 0.35) 0px -2px 6px 0px;
-    * {
-        box-sizing: border-box;
-        font-family: "Pretendard-Medium";
-        font-size: 14px;
-    }
-`;
-
-const mainContainer = css`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    /* margin: 0 auto; */
-    border-radius: 10px;
-    width: 600px;
-    height: 600px;
-    background-color: #fff;
-    box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const imgBox = css`
-    width: 300px;
-    margin: 0 auto;
-    margin-top: 70px;
-    & img {
-        width: 100%;
-    }
-`;
-
-const introText = css`
-    padding: 70px 0;
-    text-align: center;
-    color: #6f6257;
-    & p {
-        line-height: 25px;
-        font-size: 18px;
-    }
-`;
-
-const line = css`
-    width: 400px;
-    border-top: 2px solid #eee;
-`;
-
-const loginButton = css`
-    margin-top: 30px;
-    width: 200px;
-    & img {
-        margin-top: 8px;
-        width: 100%;
-    }
-`;
+import * as S from "./Style";
 
 function OAuth2Signin() {
     return (
         <>
             <Reset />
-            <div css={mainLayout}>
-                <div css={mainContainer}>
-                    <div css={imgBox}>
+            <div css={S.mainLayout}>
+                <div css={S.mainContainer}>
+                    <div css={S.imgBox}>
                         <img src="/assets/images/logo.png" alt="" />
                     </div>
-                    <div css={introText}>
+                    <div css={S.introText}>
                         <p>
                             우리를 기억하는 메모리어스, 지금 시작해보세요.
                             <br />
                             아래의 버튼을 눌러 카카오 계정 혹은 네이버 계정으로 로그인해주세요
                         </p>
                     </div>
-                    <div css={line}>{"   "}</div>
-                    <div css={loginButton}>
-                        <img src="/assets/images/kakao_login_logo.png" alt="" />
-                        <img src="/assets/images/naver_login_logo.png" alt="" />
+                    <div css={S.line}>{"   "}</div>
+                    <div css={S.loginButtonBox}>
+                        <button css={S.loginButton}>
+                            <img src="/assets/images/kakao_login_logo.png" alt="" />
+                        </button>
+                        <button css={S.loginButton}>
+                            <img src="/assets/images/naver_login_logo.png" alt="" />
+                        </button>
                     </div>
                 </div>
             </div>
