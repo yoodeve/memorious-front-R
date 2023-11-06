1fb395a0379237fb5b6c0aeca7b71b1f3302a581
SeongGwangJu <seonggwangdev@gmail.com>
2023-11-06 13:32:12 +0900

    feat: dropdown on '설정'
    dropdown 추가 및 navigate

--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Dropdown } from "antd";
 import { sidebarMenuList } from "./sidebarMenu";
 import { bottomSettingMenuBox, groupBox, groupBoxWrapper, imageBox, sideBarLabel, sidebarContainer } from "./style";
 /** @jsxImportSource @emotion/react */
@@ -11,6 +12,21 @@ function Sidebar() {
     const navigateToPage = route => () => {
         navigate(route);
     };
+
+    const items = [
+        {
+            key: "1",
+            label: <span onClick={() => navigate("/setting/myfamily")}>가족페이지</span>,
+        },
+        {
+            key: "2",
+            label: <span onClick={() => navigate("/setting/mypage")}>마이페이지</span>,
+        },
+        {
+            key: "3",
+            label: <span>가족초대하기</span>,
+        },
+    ];
 
     return (
         <div css={sidebarContainer}>
@@ -29,7 +45,9 @@ function Sidebar() {
             <div css={bottomSettingMenuBox}>
                 <span className="my-label">My프로필</span>
                 <div className="right-titles">
-                    <span onClick={() => navigate("/setting/mypage")}>설정</span>
+                    <Dropdown menu={{ items }} placement="topRight" arrow={{ pointAtCenter: true }}>
+                        <span>설정</span>
+                    </Dropdown>
                     <span>로그아웃</span>
                 </div>
             </div>
