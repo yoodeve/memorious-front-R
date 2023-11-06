eb12e3a41896ac50e4bdff1213f59a18df1ccabd
yoodeve <janhold@naver.com>
2023-11-06 12:09:01 +0900

    Chore: antd 테마색상 변경

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ConfigProvider } from "antd";
 import App from "./App";
 
-/** @todo 컬러 옵션 : https://ant.design/docs/react/customize-theme */
-const config = {
-    token: {
-        colorPrimary: "#6F6257",
-        colorWarning: "#952323",
-        colorError: "#952323",
-        colorInfo: "#e6a156",
-        colorTextBase: "#666",
-    },
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <ConfigProvider theme={config}>
-        <App />
-    </ConfigProvider>,
-);
+
+root.render(<App />);
