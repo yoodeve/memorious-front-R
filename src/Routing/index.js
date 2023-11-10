6dfde62aed9bc0b42f54685ab224fdd75477353a
JuyeongWoo <dlsrtyex63@gmail.com>
2023-11-10 16:41:04 +0900

    Feat: BoardList 컴포넌트 추가

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main/Main";
 import SideBar from "../pages/SideBar/SideBar";
 import GlobalStyle from "../style/Global";
+import BoardList from "../pages/BoardList/BoardList";
 
 function Routing() {
     return (
@@ -13,7 +14,7 @@ function Routing() {
                     <Route path="/" element={<SideBar />}>
                         <Route path="/calendar" element={<Main />} />
                         <Route path="/memo" element={<>메모</>} />
-                        <Route path="/board" element={<>게시판</>} />
+                        <Route path="/board" element={<BoardList />} />
                         <Route path="/check-list" element={<>체크</>} />
                         <Route path="/chart" element={<>차트</>} />
                         <Route path="/map" element={<>맵</>} />
