87be8324c6c05b9c2c7d1de2ab4a98e793ffe218
yoodeve <janhold@naver.com>
2023-11-13 09:27:13 +0900

    Feat: chart api 부착작업 진행중

--- src/component/Chart/ChartGraph.jsx
+++ src/component/Chart/ChartGraph.jsx
@@ -1,12 +1,23 @@
-import React from "react";
-// import FbsChart from "./FbsChart";
+import React, { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import dayjs from "dayjs";
+import FbsChart from "./FbsChart";
+import { instance } from "../../config";
+import { rcUserOnChartArray } from "../../store/atoms/chartAtoms";
 // import StepChart from "./StepChart";
 // import PulseChart from "./PulseChart";
 
 function ChartGraph() {
+    const userList = useRecoilValue(rcUserOnChartArray);
+    const getChartData = () => {
+        instance.post("/api/chart/graph", { userList: [...userList], startDate: dayjs("2023-11-09") });
+    };
+    useEffect(() => {
+        getChartData();
+    }, [userList]);
     return (
         <>
-            {/* <FbsChart /> */}
+            <FbsChart />
             {/* <StepChart /> */}
             {/* <PulseChart /> */}
         </>
--- src/component/Chart/FbsChart.jsx
+++ src/component/Chart/FbsChart.jsx
@@ -20,49 +20,44 @@ function FbsChart() {
     }, [userList]);
 
     useEffect(() => {
-        console.log("dataObjec>>>", data);
-        console.log("userList>>>", userList);
         if (userList.length > 0 && data.length > 0) {
-            const formatTime = d3.timeFormat("%m-%d");
-            const w = 350;
-            const h = 120;
-            const startDate = dayjs(data.map(e => e.startDate));
-            const endDate = dayjs(startDate).add(7, "day");
-            const dataArray = userList.map(name => {
-                return data.map(el => {
-                    return el[name];
-                });
-            });
-
-            const maxYValue = Math.max([...dataArray]);
-
-            const svg = d3.select(chartRef.current).attr("width", w).attr("height", h).style("background", "#fff").style("margin-top", 40).style("overflow", "visible");
-            const xScale = d3.scaleTime().domain([startDate, endDate]).range([0, w]);
-            const yScale = d3.scaleLinear().domain([0, maxYValue]).range([h, 0]);
-            const xAxis = d3.axisBottom(xScale).tickFormat(formatTime);
-            const yAxis = d3.axisLeft(yScale).tickSize(20);
-
-            svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`);
-            svg.append("g").call(yAxis);
-            if (dataArray.length > 0) {
-                const generateLine = d3
-                    .line()
-                    .x((d, i) => xScale(i))
-                    .y(yScale)
-                    .curve(d3.curveCardinal);
-                dataArray.forEach((e, index) => {
-                    console.log(dataArray);
-                    svg.append("path")
-                        .data([e])
-                        .attr("d", d => {
-                            return generateLine(d);
-                        })
-                        .attr("fill", "none")
-                        .attr("stroke-width", 2)
-                        // eslint-disable-next-line no-nested-ternary
-                        .attr("stroke", `#${index % 3 === 0 ? "666666" : index % 3 === 1 ? "e6a156" : "952323"}`);
-                });
-            }
+            // const formatTime = d3.timeFormat("%m-%d");
+            // const w = 350;
+            // const h = 120;
+            // const startDate = dayjs(data.map(e => e.startDate));
+            // const endDate = dayjs(startDate).add(7, "day");
+            // const dataArray = userList.map(name => {
+            //     return data.map(el => {
+            //         return el[name];
+            //     });
+            // });
+            // const maxYValue = Math.max([...dataArray]);
+            // const svg = d3.select(chartRef.current).attr("width", w).attr("height", h).style("background", "#fff").style("margin-top", 40).style("overflow", "visible");
+            // const xScale = d3.scaleTime().domain([startDate, endDate]).range([0, w]);
+            // const yScale = d3.scaleLinear().domain([0, maxYValue]).range([h, 0]);
+            // const xAxis = d3.axisBottom(xScale).tickFormat(formatTime);
+            // const yAxis = d3.axisLeft(yScale).tickSize(20);
+            // svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`);
+            // svg.append("g").call(yAxis);
+            // if (dataArray.length > 0) {
+            //     const generateLine = d3
+            //         .line()
+            //         .x((d, i) => xScale(i))
+            //         .y(yScale)
+            //         .curve(d3.curveCardinal);
+            //     dataArray.forEach((e, index) => {
+            //         console.log(dataArray);
+            //         svg.append("path")
+            //             .data([e])
+            //             .attr("d", d => {
+            //                 return generateLine(d);
+            //             })
+            //             .attr("fill", "none")
+            //             .attr("stroke-width", 2)
+            //             // eslint-disable-next-line no-nested-ternary
+            //             .attr("stroke", `#${index % 3 === 0 ? "666666" : index % 3 === 1 ? "e6a156" : "952323"}`);
+            //     });
+            // }
         }
     }, [chartRef, data]);
 
