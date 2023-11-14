8651424b084dd0b4d5f4cdd36be46d5b5466a148
SeongGwangJu <seonggwangdev@gmail.com>
2023-11-15 04:49:09 +0900

    chore: modify margin between components

--- src/component/Calendar/Modal/AddModal/StyledComponents/style.js
+++ src/component/Calendar/Modal/AddModal/StyledComponents/style.js
@@ -7,19 +7,13 @@ export const SModal = styled(Modal)`
     }
     .ant-modal-title {
         font-size: 26px;
-        margin-bottom: 20px;
-    }
-    & h1 {
-        font-size: 18px;
-        font-weight: 600;
     }
 `;
 
 export const STitleInput = styled(Input)`
+    width: 380px;
     font-size: 24px;
     font-weight: 700;
-    margin: 20px 0px;
-    width: 380px;
 `;
 
 export const SColorPicker = styled(ColorPicker)`
@@ -37,18 +31,28 @@ export const SCheckbox = styled(Checkbox)`
     font-size: 22px;
     font-weight: 700;
 `;
+
 export const STimePicker = styled(TimePicker)`
-    margin: 20px 0px;
     font-size: 20px;
     font-weight: 600;
 
+    .ant-picker {
+        border: 1px 0px 1px 1px solid #d9d9d9 !important;
+    }
+    .ant-picker-input > input {
+        border: 1px 0px 1px 1px solid #d9d9d9 !important;
+    }
+
+    .anticon-close-circle {
+        display: none !important;
+    }
     /* todo
     Ok 버튼 지우기 */
 `;
 
 export const SAttendeeSelect = styled(Select)`
-    margin: 20px 0px;
     width: 400px;
+    margin-bottom: 20px;
     font-size: 16px !important;
     font-weight: 600;
 `;
@@ -56,44 +60,46 @@ export const SAttendeeSelect = styled(Select)`
 export const SSelectOption = styled(Select.Option)``;
 
 export const SLocationInput = styled(Input)`
+    width: 400px;
     margin-bottom: 20px;
     font-size: 16px;
     font-weight: 600;
-    width: 400px;
 `;
+
 export const SCycleSelect = styled(Select)`
     width: 400px;
-    margin-bottom: 20px;
+    margin-bottom: 10px;
 `;
 
 export const SCycleInput = styled(Input)`
     width: 40px;
-    margin-left: 20px;
-    font-size: 18px;
-    height: 40px;
-`;
-export const SDescriptionInput = styled(Input.TextArea)`
-    margin-bottom: 20px;
-    font-size: 16px;
-    font-weight: 600;
-    width: 400px;
+    margin: 0px 10px 0px 20px;
 `;
 
-export const SRadioRepeat = styled(Radio.Group)`
-    margin: 5px 0px;
+export const SRadioGroup = styled(Radio.Group)`
+    margin: 0px 0px;
 `;
+
 export const SRadio = styled(Radio)`
-    margin: 5px 0px;
-    font-weight: 600;
-    font-size: 18px;
+    margin-top: 100px 0px;
+    font-weight: 500;
+    font-size: 14px;
 `;
 
 export const SDatePicker = styled(DatePicker)`
-    margin-right: 20px;
+    width: 110px;
     .anticon-close-circle {
         display: none !important;
     }
 `;
+
 export const SRepeatInput = styled(Input)`
     width: 40px;
+`;
+
+export const SDescriptionInput = styled(Input.TextArea)`
+    width: 400px;
+    margin-top: 10px;
+    font-size: 16px;
+    font-weight: 600;
 `;
