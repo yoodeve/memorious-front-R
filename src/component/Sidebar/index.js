96d8152305a3c0d2ea0bca7bf3ae19ca9eceed56
SeongGwangJu <seonggwangdev@gmail.com>
2023-11-14 15:13:28 +0900

    Merge branch '8-memorioussetting-my-pblsh' into main

--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { Dropdown } from "antd";
 import { sidebarMenuList } from "./sidebarMenu";
 import { bottomSettingMenuBox, groupBox, groupBoxWrapper, imageBox, sideBarLabel, sidebarContainer } from "./style";
 /** @jsxImportSource @emotion/react */
@@ -12,6 +13,21 @@ function SidebarContainer() {
     const navigateToPage = route => () => {
         navigate(route);
     };
+
+    const items = [
+        {
+            key: "1",
+            label: <span onClick={() => navigate("/setting/myfamily")}>가족페이지</span>,
+        },
+        {
+            key: "2",
+            label: <span onClick={() => navigate("/setting/mypage")}>마이페이지</span>,
+        },
+        {
+            key: "3",
+            label: <span>가족초대하기</span>,
+        },
+    ];
 
     return (
         <div css={sidebarContainer}>
@@ -30,9 +46,11 @@ function SidebarContainer() {
                 </div>
             </div>
             <div css={bottomSettingMenuBox}>
-                <span className="my-label">마이프로필</span>
-                <div className="right-titles" onClick={() => navigate("/setting/mypage")}>
-                    <span>설정</span>
+                <span className="my-label">My프로필</span>
+                <div className="right-titles">
+                    <Dropdown menu={{ items }} placement="topRight" arrow={{ pointAtCenter: true }}>
+                        <span>설정</span>
+                    </Dropdown>
                     <span>로그아웃</span>
                 </div>
             </div>
