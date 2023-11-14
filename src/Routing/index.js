3977249b4aac6926e5137f0501143c1dd1b3c868
yoodeve <janhold@naver.com>
2023-11-14 09:42:10 +0900

    feat: TokenLayout삭제 및 memo 무한스크롤 수정(진행중)

--- src/Routing/index.js
+++ src/Routing/index.js
@@ -8,7 +8,6 @@ import MemoRoute from "./MemoRoute";
 import SettingRoute from "./SettingRoute";
 import CreateFamily from "../pages/CreateFamily/CreateFamily";
 import Oauth2Route from "./Oauth2Route";
-import TokenLayout from "../component/TokenLayout";
 
 function Routing() {
     return (
@@ -16,21 +15,18 @@ function Routing() {
             <GlobalStyle />
             <BrowserRouter>
                 <Routes>
-                    <Route element={<TokenLayout />}>
-                        <Route element={<SideBar />}>
-                            <Route path="/" element={<Navigate replace to="calendar" />} />
-                            <Route path="calendar/*" element={<CalendarPage />} />
-                            <Route path="memo" element={<MemoRoute />} />
-                            <Route path="memo/*" element={<MemoRoute />} />
-                            <Route path="board" element={<>게시판</>} />
-                            <Route path="check-list" element={<>체크</>} />
-                            <Route path="chart/*" element={<ChartRoute />} />
-                            <Route path="map" element={<>맵</>} />
-                            <Route path="setting/*" element={<SettingRoute />} />
-                        </Route>
-                        <Route path="/auth/oauth2/*" element={<Oauth2Route />} />
-                        <Route path="/create/family" element={<CreateFamily />} />
+                    <Route element={<SideBar />}>
+                        <Route path="/" element={<Navigate replace to="calendar" />} />
+                        <Route path="calendar/*" element={<CalendarPage />} />
+                        <Route path="memo/*" element={<MemoRoute />} />
+                        <Route path="board" element={<>게시판</>} />
+                        <Route path="check-list" element={<>체크</>} />
+                        <Route path="chart/*" element={<ChartRoute />} />
+                        <Route path="map" element={<>맵</>} />
+                        <Route path="setting/*" element={<SettingRoute />} />
                     </Route>
+                    <Route path="/auth/oauth2/*" element={<Oauth2Route />} />
+                    <Route path="/create/family" element={<CreateFamily />} />
                 </Routes>
             </BrowserRouter>
         </>
