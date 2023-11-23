a9b7177178f64e93a300dae7221cbb21bcb77487
yoodeve <janhold@naver.com>
2023-11-23 15:43:14 +0900

    Feat: 차트 테이블 오류값 처리
    - 차트 테이블 숫자 정규식 추가
    - 0값 입력 시 토스트메시지 노출되게 추가

--- src/component/ChartDetail/ChartDetailAddBtn.jsx
+++ src/component/ChartDetail/ChartDetailAddBtn.jsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, notification } from "antd";
 import React from "react";
 import { useMutation, useQueryClient } from "react-query";
 import { useRecoilState } from "recoil";
@@ -8,6 +8,7 @@ import { instance } from "../../config";
 
 function ChartDetailAddBtn() {
     const queryClient = useQueryClient();
+    const [api, contextHolder] = notification.useNotification();
     const { data } = queryClient.getQueryState(["getPrincipal"]);
     const mutationEdit = useMutation(d => instance.post("/api/chart", d), {
         onSuccess: () => {
@@ -16,11 +17,26 @@ function ChartDetailAddBtn() {
     });
     const [tableData, setTableData] = useRecoilState(rcTableData);
 
+    const openNotificationWithIcon = t => {
+        api[t]({
+            message: "에러",
+            description: "올바른 정보를 입력해주세요.",
+        });
+    };
     const onClick = async () => {
+        if (Object.values(tableData).includes(0)) {
+            openNotificationWithIcon("warning");
+            return;
+        }
         await mutationEdit.mutate({ ...tableData, userId: data.data.userId });
         setTableData({ step: 0, fbs: 0, date: dayjs().format("YYYY-MM-DD"), pulse: 0 });
     };
-    return <Button onClick={onClick}>추가</Button>;
+    return (
+        <>
+            {contextHolder}
+            <Button onClick={onClick}>추가</Button>
+        </>
+    );
 }
 
 export default ChartDetailAddBtn;
